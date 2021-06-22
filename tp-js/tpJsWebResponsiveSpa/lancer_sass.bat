39a52af6e09de031a2852d02f507721730523b8e
didier <didier@d-defrance.fr>
2021-06-22 09:19:07 +0200

    responsiveSpa

---
+++ tp-js/tpJsWebResponsiveSpa/lancer_sass.bat
@@ -0,0 +1,5 @@
+REM prealablement: npm install -g sass
+REM sass scss/styles.scss css/styles.css
+REM sass scss/my-custom-bootstrap.scss css/my-custom-bootstrap.css
+sass scss/avec-transparence.scss css/avec-transparence.css
+pause
